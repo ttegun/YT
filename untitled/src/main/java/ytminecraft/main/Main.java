94c03704b419095bb12116be8041e6519e0d266b
Lucent <ttegun11@gmail.com>
2024-12-26 17:40:53 +0900

    웰컴 타이틀 추가 채팅 칭호 추가 출석체크 추가 칭호상점 추가 출석코인, 현재시각 확인 스코어보드 추가

--- untitled/src/main/java/ytminecraft/main/Main.java
+++ untitled/src/main/java/ytminecraft/main/Main.java
@@ -2,8 +2,15 @@ package ytminecraft.main;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class Main extends JavaPlugin {
+
+    private ScoreboardManager scoreboardManager;
+    private Map<Player, Integer> attendanceCoinsMap;
 
     @Override
     public void onEnable() {
@@ -11,6 +18,9 @@ public final class Main extends JavaPlugin {
         TitleManager titleManager = new TitleManager();
         AttendanceManager attendanceManager = new AttendanceManager();
         ShopManager shopManager = new ShopManager();
+        scoreboardManager = new ScoreboardManager(this);
+        attendanceCoinsMap = new HashMap<>();
+
         Bukkit.getPluginManager().registerEvents(new Welcome(), this);
         Bukkit.getPluginManager().registerEvents(titleManager, this);
         Bukkit.getPluginManager().registerEvents(shopManager, this);
@@ -18,10 +28,26 @@ public final class Main extends JavaPlugin {
         this.getCommand("한글닉").setExecutor(titleManager);
         this.getCommand("출석체크").setExecutor(attendanceManager);
         this.getCommand("상점").setExecutor(shopManager);
+
+        // Example usage: create scoreboard for all online players
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            int attendanceCoins = getAttendanceCoins(player); // Retrieve attendance coins for the player
+            scoreboardManager.createScoreboard(player, attendanceCoins);
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private int getAttendanceCoins(Player player) {
+        // Retrieve the attendance coins for the player from the map
+        return attendanceCoinsMap.getOrDefault(player, 0);
+    }
+
+    // Example method to add attendance coins for a player
+    public void addAttendanceCoins(Player player, int coins) {
+        attendanceCoinsMap.put(player, attendanceCoinsMap.getOrDefault(player, 0) + coins);
     }
 }
---
+++ untitled/src/main/java/ytminecraft/main/ScoreboardManager.java
@@ -0,0 +1,41 @@
+package ytminecraft.main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ScoreboardManager {
+
+    private final JavaPlugin plugin;
+
+    public ScoreboardManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void createScoreboard(Player player, int attendanceCoins) {
+        org.bukkit.scoreboard.ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+
+        // Create the objective
+        Objective objective = board.registerNewObjective("info", "dummy", ChatColor.RED + "" + ChatColor.BOLD + "유튜브 소통방");
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+        // Set the current real-world time
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String currentTime = sdf.format(new Date());
+        Score timeScore = objective.getScore(ChatColor.GREEN + "현실시각: " + ChatColor.WHITE + currentTime);
+        timeScore.setScore(2);
+
+        // Set the attendance coins
+        Score coinsScore = objective.getScore(ChatColor.GREEN + "출석코인: " + ChatColor.WHITE + attendanceCoins);
+        coinsScore.setScore(1);
+
+        // Assign the scoreboard to the player
+        player.setScoreboard(board);
+    }
+}
